a201da817b91b7150a515c55ed9a7830eb338435
Brian Miller <bmiller@southcomm.com>
2019-12-02 14:22:46 -0600

    update grid innovations footer, header and full width blocks

--- tenants/tdworld/config/email-x.js
+++ tenants/tdworld/config/email-x.js
@@ -2,4 +2,14 @@ const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config'
 
 const config = new EmailXConfiguration('https://ebm.serve.email-x.io');
 
+config
+  .setAdUnits('grid-innovations', [
+    {
+      name: 'leaderboard_primary',
+      id: '5ddbfb17ec515565e49a82d3',
+      width: 670,
+      height: 90,
+    },
+  ]);
+
 module.exports = config;
