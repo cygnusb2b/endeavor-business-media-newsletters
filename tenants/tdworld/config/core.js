7d6775c686ba7eb6481f5311816bf2d1aa854185
Brian Miller <bmiller@southcomm.com>
2019-12-03 12:51:38 -0600

    configure opt out phone number

--- tenants/tdworld/config/core.js
+++ tenants/tdworld/config/core.js
@@ -2,4 +2,5 @@ module.exports = {
   optOutAddress: 'Transmission and Distribution World<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
   optOutSafeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
   optOutPrivacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
+  optOutPhoneNumber: '800-547-7377',
 };
