d1d64b053315ab14759047cacc8ef28dfff5873c
Brian Miller <bmiller@southcomm.com>
2019-12-04 11:59:22 -0600

    update tenant name in opt out address

--- tenants/ewweb/config/core.js
+++ tenants/ewweb/config/core.js
@@ -1,6 +1,6 @@
 module.exports = {
   optOut: {
-    address: 'Transmission and Distribution World<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
+    address: 'Electrical Wholesaling<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
