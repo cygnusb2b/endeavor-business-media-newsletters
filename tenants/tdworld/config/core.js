ebdb48744c87189cda809456fae28452337aad22
Brian Miller <bmiller@southcomm.com>
2019-12-04 17:17:35 -0600

    add social links

--- tenants/tdworld/config/core.js
+++ tenants/tdworld/config/core.js
@@ -5,4 +5,9 @@ module.exports = {
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
   },
+  socialLink: {
+    twitter: 'https://twitter.com/tdworldmag?elqTrack=true',
+    facebook: 'https://www.facebook.com/pages/Transmission-Distribution-World/110964078942390?elqTrack=true',
+    linkedIn: 'https://www.linkedin.com/groups/Transmission-Distribution-World-4078692?elqTrack=true',
+  },
 };
