6098ec4ed73f5fd2e7b64c098e2114a24391ad57
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2019-12-04 17:11:57 -0600

    Updated SafeSender email for ECM

--- tenants/ecmweb/config/core.js
+++ tenants/ecmweb/config/core.js
@@ -1,7 +1,7 @@
 module.exports = {
   optOut: {
     address: 'EC&M<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
-    safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
+    safeSenders: 'enews.ecmweb.com and mail.ecmweb.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
   },
