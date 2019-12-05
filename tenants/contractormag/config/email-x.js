f5f40b9f823fe667bc2fb1c938b11541feb8dc2d
Brian Miller <bmiller@southcomm.com>
2019-12-05 09:36:53 -0600

    copy from ewweb

---
+++ tenants/contractormag/config/email-x.js
@@ -0,0 +1,13 @@
+const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config');
+
+const config = new EmailXConfiguration('https://ebm.serve.email-x.io');
+config
+  .setAdUnits('ew-update', [
+    {
+      name: 'leaderboardPrimary',
+      id: '5de7f1ddd3007738c2f2249b',
+      width: 670,
+      height: 90,
+    },
+  ]);
+module.exports = config;
