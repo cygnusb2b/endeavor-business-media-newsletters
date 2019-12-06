a3b27c4ced4b476f407928c15c387e059c9a4343
Brian Miller <bmiller@southcomm.com>
2019-12-06 10:23:30 -0600

    udpate alias to just update

--- tenants/ewweb/config/email-x.js
+++ tenants/ewweb/config/email-x.js
@@ -2,7 +2,7 @@ const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config'
 
 const config = new EmailXConfiguration('https://ebm.serve.email-x.io');
 config
-  .setAdUnits('ew-update', [
+  .setAdUnits('update', [
     {
       name: 'leaderboardPrimary',
       id: '5de7f1ddd3007738c2f2249b',
