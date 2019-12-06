4bc4b1a3615f7e35b30f5f3e5f1f0de4334bb7db
Brian Miller <bmiller@southcomm.com>
2019-12-06 09:21:11 -0600

    emailx

--- tenants/contractingbusiness/config/email-x.js
+++ tenants/contractingbusiness/config/email-x.js
@@ -2,10 +2,26 @@ const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config'
 
 const config = new EmailXConfiguration('https://ebm.serve.email-x.io');
 config
-  .setAdUnits('ew-update', [
+  .setAdUnits('hvac-talk', [
     {
       name: 'leaderboardPrimary',
-      id: '5de7f1ddd3007738c2f2249b',
+      id: '5dea7156d300774ccaf24d3c',
+      width: 670,
+      height: 90,
+    },
+  ])
+  .setAdUnits('hvacr-hotmail', [
+    {
+      name: 'leaderboardPrimary',
+      id: '5dea7198d3007741d5f24d7c',
+      width: 670,
+      height: 90,
+    },
+  ])
+  .setAdUnits('refrigerant-4-1-1', [
+    {
+      name: 'leaderboardPrimary',
+      id: '5dea71cd76787af06711cb5b',
       width: 670,
       height: 90,
     },
