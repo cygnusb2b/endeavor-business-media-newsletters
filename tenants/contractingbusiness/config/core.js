e7727bcd07c3bb3713d90c3816832c7d6b975a27
Brian Miller <bmiller@southcomm.com>
2019-12-06 09:12:54 -0600

    update core config

--- tenants/contractingbusiness/config/core.js
+++ tenants/contractingbusiness/config/core.js
@@ -1,6 +1,6 @@
 module.exports = {
   optOut: {
-    address: 'Electrical Wholesaling<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
+    address: 'Contracting Business<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
@@ -8,17 +8,17 @@ module.exports = {
   socialMediaLinks: [
     {
       provider: 'twitter',
-      href: 'https://twitter.com/ElecWholesaling?elqTrack=true',
+      href: 'https://twitter.com/contractingbiz/?elqTrack=true',
       title: 'Follow us on Twitter',
     },
     {
       provider: 'facebook',
-      href: 'https://www.facebook.com/ElectricalConstructionMaintenanceMagazine?elqTrack=true',
+      href: 'https://www.facebook.com/CBMag/?elqTrack=true',
       title: 'Follow us on Facebook',
     },
     {
       provider: 'linkedin',
-      href: 'https://www.linkedin.com/groups/2685562?elqTrack=true',
+      href: 'https://www.linkedin.com/groups/4078340/profile?elqTrack=true',
       title: 'Follow us on LinkedIn',
     },
   ],
