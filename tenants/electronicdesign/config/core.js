8734144aaa78a7a0d128d03e7b9a0f628dcc7bef
Brian Miller <bmiller@southcomm.com>
2019-12-08 12:15:29 -0600

    udpated core config

--- tenants/electronicdesign/config/core.js
+++ tenants/electronicdesign/config/core.js
@@ -1,6 +1,6 @@
 module.exports = {
   optOut: {
-    address: 'Electrical Marketing<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
+    address: 'Electronic Design<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
@@ -8,13 +8,18 @@ module.exports = {
   socialMediaLinks: [
     {
       provider: 'twitter',
-      href: 'https://twitter.com/truckerAMT?elqTrack=true',
+      href: 'https://twitter.com/ElectronicDesgn?elqTrack=true',
       title: 'Follow us on Twitter',
     },
     {
       provider: 'facebook',
-      href: 'https://www.facebook.com/pages/American-Trucker-Magazine/37172697238?elqTrack=true',
+      href: 'https://www.facebook.com/ElectronicDesign?elqTrack=true',
       title: 'Follow us on Facebook',
+    },
+    {
+      provider: 'linkedin',
+      href: 'https://www.linkedin.com/groups/4210549/?elqTrack=true',
+      title: 'Follow us on LinkedIn',
     },
   ],
 };
