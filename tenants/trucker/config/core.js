7e8fc0d180cdef629afbbac5540d3aa2c01a3115
Brian Miller <bmiller@southcomm.com>
2019-12-08 12:15:18 -0600

    fix tenant name

--- tenants/trucker/config/core.js
+++ tenants/trucker/config/core.js
@@ -1,6 +1,6 @@
 module.exports = {
   optOut: {
-    address: 'Electrical Marketing<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
+    address: 'American Trucker<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
