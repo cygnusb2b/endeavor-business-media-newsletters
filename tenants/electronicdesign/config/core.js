0bb09261f09284e4d281dc75c9f0ed743d33fe98
Brian Miller <bmiller@southcomm.com>
2019-12-08 11:59:21 -0600

    setup electronicdesign

---
+++ tenants/electronicdesign/config/core.js
@@ -0,0 +1,20 @@
+module.exports = {
+  optOut: {
+    address: 'Electrical Marketing<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
+    safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
+    privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
+    phoneNumber: '800-547-7377',
+  },
+  socialMediaLinks: [
+    {
+      provider: 'twitter',
+      href: 'https://twitter.com/truckerAMT?elqTrack=true',
+      title: 'Follow us on Twitter',
+    },
+    {
+      provider: 'facebook',
+      href: 'https://www.facebook.com/pages/American-Trucker-Magazine/37172697238?elqTrack=true',
+      title: 'Follow us on Facebook',
+    },
+  ],
+};
