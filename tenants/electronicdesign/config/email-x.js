ef534309950878d2ed2a3dbe5bfa1e447e224723
Brian Miller <bmiller@southcomm.com>
2019-12-08 12:29:10 -0600

    add ad units

--- tenants/electronicdesign/config/email-x.js
+++ tenants/electronicdesign/config/email-x.js
@@ -2,10 +2,18 @@ const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config'
 
 const config = new EmailXConfiguration('https://ebm.serve.email-x.io');
 config
-  .setAdUnits('daily', [
+  .setAdUnits('analog-power-source', [
     {
       name: 'leaderboardPrimary',
-      id: '5ded36dbd30077d1dff275b3',
+      id: '5ded409dd300776c6af275f4',
+      width: 670,
+      height: 90,
+    },
+  ])
+  .setAdUnits('electronic-design-daily', [
+    {
+      name: 'leaderboardPrimary',
+      id: '5ded40d076787ae65c11e341',
       width: 670,
       height: 90,
     },
