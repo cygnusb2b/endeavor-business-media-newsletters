ffc46e09313a7afa34d871594d932ae3d8f2ae0a
Brian Miller <bmiller@southcomm.com>
2019-12-08 11:46:33 -0600

    udpate email x config

--- tenants/trucker/config/email-x.js
+++ tenants/trucker/config/email-x.js
@@ -2,10 +2,10 @@ const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config'
 
 const config = new EmailXConfiguration('https://ebm.serve.email-x.io');
 config
-  .setAdUnits('electrical-marketing-update', [
+  .setAdUnits('daily', [
     {
       name: 'leaderboardPrimary',
-      id: '5dec0f3e76787a351111e2b3',
+      id: '5ded36dbd30077d1dff275b3',
       width: 670,
       height: 90,
     },
