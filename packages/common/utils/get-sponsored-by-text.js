47f93088db200bbd56693d84fb3bbf1c6ebc5cb0
Brian Miller <bmiller@southcomm.com>
2019-12-09 09:13:25 -0600

    dont need company if it isnt sponsored

--- packages/common/utils/get-sponsored-by-text.js
+++ packages/common/utils/get-sponsored-by-text.js
@@ -3,7 +3,7 @@ const { get } = require('@base-cms/object-path');
 module.exports = (node, fallbackValue = '') => {
   const { labels = [] } = node;
   const sponsored = labels.includes('Sponsored');
-  const companyName = get(node, 'company.name');
   if (!sponsored) return fallbackValue;
+  const companyName = get(node, 'company.name');
   return companyName ? `Sponsored by ${companyName}` : 'Sponsored';
 };
