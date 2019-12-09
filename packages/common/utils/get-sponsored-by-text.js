887dec8a93a562c6b85c8148574e552bf261e6ea
Brian Miller <bmiller@southcomm.com>
2019-12-09 08:58:57 -0600

    update util logic

--- packages/common/utils/get-sponsored-by-text.js
+++ packages/common/utils/get-sponsored-by-text.js
@@ -1,11 +1,9 @@
 const { get } = require('@base-cms/object-path');
 
-module.exports = (node, fallbackValue) => {
+module.exports = (node, fallbackValue = '') => {
   const { labels = [] } = node;
   const sponsored = labels.includes('Sponsored');
   const companyName = get(node, 'company.name');
-  if (sponsored) {
-    return companyName ? `Sponsored by ${node.company.name}` : 'Sponsored';
-  }
-  return fallbackValue;
+  if (!sponsored) return fallbackValue;
+  return companyName ? `Sponsored by ${node.company.name}` : 'Sponsored';
 };
