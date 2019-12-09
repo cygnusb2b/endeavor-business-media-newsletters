05b09b5394a765cc9ee867b328d25bc19ec8e777
Brian Miller <bmiller@southcomm.com>
2019-12-09 10:27:33 -0600

    add industry insight newsletter

--- tenants/newequipment/config/core.js
+++ tenants/newequipment/config/core.js
@@ -1,6 +1,6 @@
 module.exports = {
   optOut: {
-    address: 'Electrical Marketing<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
+    address: 'NED Insight<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
@@ -8,17 +8,17 @@ module.exports = {
   socialMediaLinks: [
     {
       provider: 'twitter',
-      href: 'https://twitter.com/electricalmarketing?elqTrack=true',
+      href: 'https://twitter.com/?elqTrack=true#!/newequipment',
       title: 'Follow us on Twitter',
     },
     {
       provider: 'facebook',
-      href: 'https://www.facebook.com/elecwholesalingmag?elqTrack=true',
+      href: 'https://www.facebook.com/NewEquipment?elqTrack=true',
       title: 'Follow us on Facebook',
     },
     {
       provider: 'linkedin',
-      href: 'https://www.linkedin.com/groups2187051?elqTrack=true',
+      href: 'https://www.linkedin.com/grp/home?gid=4850947&elqTrack=true',
       title: 'Follow us on LinkedIn',
     },
   ],
--- tenants/newequipment/config/email-x.js
+++ tenants/newequipment/config/email-x.js
@@ -2,10 +2,10 @@ const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config'
 
 const config = new EmailXConfiguration('https://ebm.serve.email-x.io');
 config
-  .setAdUnits('electrical-marketing-update', [
+  .setAdUnits('industry-insider', [
     {
       name: 'leaderboardPrimary',
-      id: '5dec0f3e76787a351111e2b3',
+      id: '5dee736c76787a7d5511e6c5',
       width: 670,
       height: 90,
     },
