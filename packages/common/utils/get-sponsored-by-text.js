44904c526e1cba6a48881606eca10348f04e0409
Brian Miller <bmiller@southcomm.com>
2019-12-09 08:55:55 -0600

    add get to util

--- packages/common/utils/get-sponsored-by-text.js
+++ packages/common/utils/get-sponsored-by-text.js
@@ -1,8 +1,11 @@
+const { get } = require('@base-cms/object-path');
+
 module.exports = (node, fallbackValue) => {
   const { labels = [] } = node;
   const sponsored = labels.includes('Sponsored');
+  const companyName = get(node, 'company.name');
   if (sponsored) {
-    return node.company ? `Sponsored by ${node.company.name}` : 'Sponsored';
+    return companyName ? `Sponsored by ${node.company.name}` : 'Sponsored';
   }
   return fallbackValue;
 };
