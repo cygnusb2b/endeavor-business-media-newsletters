181e8ebed3799fc16693802c80872d5278a0652a
Brian Miller <bmiller@southcomm.com>
2019-12-10 15:49:30 -0600

    udpate site name

--- tenants/ehstoday/config/core.js
+++ tenants/ehstoday/config/core.js
@@ -1,6 +1,6 @@
 module.exports = {
   optOut: {
-    address: 'Electrical Wholesaling<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
+    address: 'EHS Today<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
