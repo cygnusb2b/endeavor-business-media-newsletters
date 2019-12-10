f40aecba13c97f4ddb0b61b1b73849bbc09efc6a
Brian Miller <bmiller@southcomm.com>
2019-12-10 12:56:18 -0600

    udpate configs

--- tenants/trailer-bodybuilders/config/core.js
+++ tenants/trailer-bodybuilders/config/core.js
@@ -1,6 +1,6 @@
 module.exports = {
   optOut: {
-    address: 'ASU Mag<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
+    address: 'Trailer Body Builders<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
@@ -8,12 +8,12 @@ module.exports = {
   socialMediaLinks: [
     {
       provider: 'twitter',
-      href: 'https://twitter.com/ASU_mag?elqTrack=true',
+      href: 'https://twitter.com/?elqTrack=true#!/trailerbb',
       title: 'Follow us on Twitter',
     },
     {
       provider: 'facebook',
-      href: 'https://www.facebook.com/asumag?elqTrack=true',
+      href: 'https://www.facebook.com/trailerbodybuilders?elqTrack=true',
       title: 'Follow us on Facebook',
     },
     // {
