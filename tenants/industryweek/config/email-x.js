ae52138fcf7fb8bfb969e495ef8b0d44cd007dde
Brian Miller <bmiller@southcomm.com>
2019-12-10 10:39:27 -0600

    remove wrong file  & update emailX

--- tenants/industryweek/config/email-x.js
+++ tenants/industryweek/config/email-x.js
@@ -2,18 +2,10 @@ const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config'
 
 const config = new EmailXConfiguration('https://ebm.serve.email-x.io');
 config
-  .setAdUnits('rer-product-wire', [
+  .setAdUnits('quick-manufacturing-news', [
     {
       name: 'leaderboardPrimary',
-      id: '5de91461d30077f95ff22f21',
-      width: 670,
-      height: 90,
-    },
-  ])
-  .setAdUnits('rer-reports', [
-    {
-      name: 'leaderboardPrimary',
-      id: '5de7e9f8d300770b38f221de',
+      id: '5defc9f276787a1ee611fd13',
       width: 670,
       height: 90,
     },
