3e57cade1c1c6de797279c3b4f03db48fd5eee8c
Brian Miller <bmiller@southcomm.com>
2019-12-10 15:58:31 -0600

    setup Bulk Transpoerter & Bulk Logistics Trends eNL

---
+++ tenants/bulktransporter/config/core.js
@@ -0,0 +1,20 @@
+module.exports = {
+  optOut: {
+    address: 'Electrical Wholesaling<br />Endeavor Business Media<br />1233 Janesville Ave<br />Fort Atkinson, WI 53538',
+    safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
+    privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
+    phoneNumber: '800-547-7377',
+  },
+  socialMediaLinks: [
+    {
+      provider: 'twitter',
+      href: 'http://twitter.com/?elqTrack=true#!/bulktransporter',
+      title: 'Follow us on Twitter',
+    },
+    {
+      provider: 'facebook',
+      href: 'http://www.facebook.com/bulktrans?elqTrack=true',
+      title: 'Follow us on Facebook',
+    },
+  ],
+};
