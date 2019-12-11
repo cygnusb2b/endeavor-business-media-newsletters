18c551d672e5b94ea4b869143f7ba480239ed6ca
Brian Miller <bmiller@southcomm.com>
2019-12-11 10:17:37 -0600

    add newsmaker newsletter

--- tenants/mhlnews/config/email-x.js
+++ tenants/mhlnews/config/email-x.js
@@ -2,6 +2,14 @@ const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config'
 
 const config = new EmailXConfiguration('https://ebm.serve.email-x.io');
 config
+  .setAdUnits('newsmakers', [
+    {
+      name: 'leaderboardPrimary',
+      id: '5df1166bd30077b9daf2a572',
+      width: 670,
+      height: 90,
+    },
+  ])
   .setAdUnits('products-of-the-week', [
     {
       name: 'leaderboardPrimary',
@@ -10,4 +18,5 @@ config
       height: 90,
     },
   ]);
+
 module.exports = config;
