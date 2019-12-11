e26549d073163523fa2e844a1938a307be3d20e1
Brian Miller <bmiller@southcomm.com>
2019-12-11 08:33:14 -0600

    update config

--- tenants/ehstoday/config/core.js
+++ tenants/ehstoday/config/core.js
@@ -4,6 +4,8 @@ module.exports = {
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
+    manageSubscriptions: '',
+    signUp: '',
   },
   socialMediaLinks: [
     {
