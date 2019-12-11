759e5f7cffba8933c98141c7ff881d691010d388
Brian Miller <bmiller@southcomm.com>
2019-12-11 14:55:19 -0600

    add Equipment Weekly to Fleet Owner

--- tenants/fleetowner/config/email-x.js
+++ tenants/fleetowner/config/email-x.js
@@ -2,6 +2,14 @@ const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config'
 
 const config = new EmailXConfiguration('https://ebm.serve.email-x.io');
 config
+  .setAdUnits('equipment-weekly', [
+    {
+      name: 'leaderboardPrimary',
+      id: '5df1578dd30077fa36f2ae8d',
+      width: 670,
+      height: 90,
+    },
+  ])
   .setAdUnits('fleet-owner-newsline', [
     {
       name: 'leaderboardPrimary',
