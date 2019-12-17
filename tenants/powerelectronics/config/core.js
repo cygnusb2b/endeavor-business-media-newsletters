20e4dd6e39523392551eef8372c198854cc165aa
Brian Miller <bmiller@southcomm.com>
2019-12-17 11:18:07 -0600

    Add Power Electronics & product spotlight newsletter

---
+++ tenants/powerelectronics/config/core.js
@@ -0,0 +1,22 @@
+module.exports = {
+  optOut: {
+    address: 'Power Electronics<br />Endeavor Business Media<br />331 54th Avenue N.<br />Nashville, TN 37209',
+    safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
+    privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
+    phoneNumber: '800-547-7377',
+    manageSubscriptions: 'https://endeavor.dragonforms.com/PWEPrefPage?r=@{encrypted_customer_id}@&pk=NLFooter',
+    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=PWEPrefPage',
+  },
+  socialMediaLinks: [
+    {
+      provider: 'twitter',
+      href: 'https://twitter.com/PowerElecTech?elqTrack=true',
+      title: 'Follow us on Twitter',
+    },
+    {
+      provider: 'facebook',
+      href: 'https://www.facebook.com/powerelectronicstech?elqTrack=true',
+      title: 'Follow us on Facebook',
+    },
+  ],
+};
