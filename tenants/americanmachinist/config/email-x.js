2d709316e56a6ba0765a1f0474ea820c34c7c063
Brian Miller <bmiller@southcomm.com>
2019-12-17 13:47:31 -0600

    add remaining american machinist newsletters

--- tenants/americanmachinist/config/email-x.js
+++ tenants/americanmachinist/config/email-x.js
@@ -2,6 +2,22 @@ const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config'
 
 const config = new EmailXConfiguration('https://ebm.serve.email-x.io');
 config
+  .setAdUnits('highlights', [
+    {
+      name: 'leaderboardPrimary',
+      id: '5df92eebd3007707eef3284b',
+      width: 670,
+      height: 90,
+    },
+  ])
+  .setAdUnits('imts-preview', [
+    {
+      name: 'leaderboardPrimary',
+      id: '5df92f5fd300779553f328ff',
+      width: 670,
+      height: 90,
+    },
+  ])
   .setAdUnits('weekly-update', [
     {
       name: 'leaderboardPrimary',
