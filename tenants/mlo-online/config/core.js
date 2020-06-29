f5626f8b538861040e9a27cf1b82c34bfc28f694
Alysha Buselt <>
2020-06-29 15:05:58 -0400

    Publication component

--- tenants/mlo-online/config/core.js
+++ tenants/mlo-online/config/core.js
@@ -1,4 +1,5 @@
 module.exports = {
+  brandAcronym: 'MLO',
   optOut: {
     address: 'Medical Laboratory Observer<br />Endeavor Business Media<br />331 54th Avenue N.<br />Nashville, TN 37209',
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
