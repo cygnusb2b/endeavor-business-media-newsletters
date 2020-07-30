e5bbec77452535db5d8406928ca1339e16ee9723
Alysha Buselt <>
2020-07-30 12:35:39 -0400

    Initial setup of new RDH tenant
    https://www.pivotaltracker.com/story/show/173840535

---
+++ tenants/rdhmag/config/core.js
@@ -0,0 +1,50 @@
+module.exports = {
+  optOut: {
+    address: 'Registered Dental Hygienist (RDH) Magazine <br />Endeavor Business Media<br />331 54th Avenue N.<br />Nashville, TN 37209',
+    safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
+    privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
+    phoneNumber: '800-547-7377',
+    manageSubscriptions: 'https://endeavor.dragonforms.com/loading.do?omedasite=RDHPrefPage',
+    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=RDHnewpref',
+  },
+  socialMediaLinks: [
+    {
+      provider: 'twitter',
+      href: 'https://twitter.com/rdhmag?lang=en',
+      title: 'Follow us on Twitter',
+    },
+    {
+      provider: 'facebook',
+      href: 'https://www.facebook.com/RDHMagazine/',
+      title: 'Follow us on Facebook',
+    },
+    {
+      provider: 'instagram',
+      href: 'https://www.instagram.com/rdhmag/?hl=en',
+      title: 'Follow us on LinkedIn',
+    },
+    {
+      provider: 'youtube',
+      href: 'https://www.youtube.com/channel/UCtOxclLyg6O0Eb5kRXqk9sA',
+      title: 'Follow us on LinkedIn',
+    },
+  ],
+  brandNavLinks: [
+    {
+      href: 'https://www.rdhmag.com/',
+      title: 'Home',
+    },
+    {
+      href: 'https://www.rdhmag.com/contact-us',
+      title: 'Contact Us',
+    },
+    {
+      href: 'https://www.rdhmag.com/subscribe',
+      title: 'Subscribe',
+    },
+    {
+      href: 'https://www.rdhmag.com/page/advertise',
+      title: 'Advertise',
+    },
+  ],
+};
