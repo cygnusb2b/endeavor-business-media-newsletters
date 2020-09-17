b980e5ae49031d1679de5f9d21e005a4b1358d62
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-09-17 12:05:01 -0500

    Configurable opt-out fields

--- tenants/evaluationengineering/config/core.js
+++ tenants/evaluationengineering/config/core.js
@@ -4,26 +4,7 @@ module.exports = {
     safeSenders: 'news.evaluationengineering.com and mail.evaluationengineering.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
-    manageSubscriptions: 'https://endeavor.dragonforms.com/loading.do?omedasite=DECPrefPage',
-    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=DECnewpref',
   },
-  socialMediaLinks: [
-    {
-      provider: 'twitter',
-      href: 'http://www.twitter.com/EE_Engineers',
-      title: 'Follow us on Twitter',
-    },
-    {
-      provider: 'facebook',
-      href: 'http://www.facebook.com/EvaluationEngineering',
-      title: 'Follow us on Facebook',
-    },
-    {
-      provider: 'linkedin',
-      href: 'http://www.linkedin.com/groups/2192877',
-      title: 'Follow us on LinkedIn',
-    },
-  ],
   brandNavLinks: [
     {
       href: 'https://www.evaluationengineering.com/',
