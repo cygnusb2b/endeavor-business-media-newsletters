546de9962a04c8c12c2b92171cc2f7f987d138fc
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-10-01 08:10:20 -0500

    bulktransporter alphaThemeConfig setup

--- tenants/bulktransporter/config/core.js
+++ tenants/bulktransporter/config/core.js
@@ -4,19 +4,5 @@ module.exports = {
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
-    manageSubscriptions: 'https://endeavor.dragonforms.com/BKTPrefPage?r=@{encrypted_customer_id}@&pk=NLFooter',
-    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=BKTnewpref',
   },
-  socialMediaLinks: [
-    {
-      provider: 'twitter',
-      href: 'https://twitter.com/?elqTrack=true#!/bulktransporter',
-      title: 'Follow us on Twitter',
-    },
-    {
-      provider: 'facebook',
-      href: 'https://www.facebook.com/bulktrans?elqTrack=true',
-      title: 'Follow us on Facebook',
-    },
-  ],
 };
