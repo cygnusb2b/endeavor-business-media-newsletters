4c4981e6e223803a8b076e6f5929d9ef48e21cc3
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-10-01 08:10:54 -0500

    contractormag alphaThemeConfig setup

--- tenants/contractormag/config/core.js
+++ tenants/contractormag/config/core.js
@@ -4,24 +4,5 @@ module.exports = {
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
-    manageSubscriptions: 'https://endeavor.dragonforms.com/CONTRPrefPage?r=@{encrypted_customer_id}@&pk=NLFooter',
-    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=CONTRnewpref',
   },
-  socialMediaLinks: [
-    {
-      provider: 'twitter',
-      href: 'https://twitter.com/contractormag?elqTrack=true',
-      title: 'Follow us on Twitter',
-    },
-    {
-      provider: 'facebook',
-      href: 'https://www.facebook.com/CONTRACTOR-Magazine-95416372437?ref=ts&elqTrack=true',
-      title: 'Follow us on Facebook',
-    },
-    {
-      provider: 'linkedin',
-      href: 'https://www.linkedin.com/groups/4078340?elqTrack=true',
-      title: 'Follow us on LinkedIn',
-    },
-  ],
 };
