e1893235fbe6fda2a40ea354c1be0d7fe2bb86c0
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-10-01 10:46:43 -0500

    foundrymag emailThemeAlpha setup

--- tenants/foundrymag/config/core.js
+++ tenants/foundrymag/config/core.js
@@ -4,14 +4,5 @@ module.exports = {
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
-    manageSubscriptions: 'https://endeavor.dragonforms.com/FMTPrefPage?r=@{encrypted_customer_id}@&pk=NLFooter',
-    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=FMTnewpref',
   },
-  socialMediaLinks: [
-    {
-      provider: 'twitter',
-      href: 'https://twitter.com/?elqTrack=true#!/foundrymag',
-      title: 'Follow us on Twitter',
-    },
-  ],
 };
