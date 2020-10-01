3e3d22dcc2783bda040c46cbf22ba6b5fa53ceb4
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-10-01 09:57:49 -0500

    Electronic Design alphaThemeConfig setup
    These guys have a mix of different header types, so some pull the “style-b” header template that allows them to upload a single image that spans the full width of the newsletter, while others pull the “style-a” template with the date/name/description/logo (informa/gemenon-theme)

--- tenants/electronicdesign/config/core.js
+++ tenants/electronicdesign/config/core.js
@@ -4,24 +4,5 @@ module.exports = {
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
-    manageSubscriptions: 'https://endeavor.dragonforms.com/EDZPrefPage?r=@{encrypted_customer_id}@&pk=NLFooter',
-    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=EDZnewpref',
   },
-  socialMediaLinks: [
-    {
-      provider: 'twitter',
-      href: 'https://twitter.com/ElectronicDesgn?elqTrack=true',
-      title: 'Follow us on Twitter',
-    },
-    {
-      provider: 'facebook',
-      href: 'https://www.facebook.com/ElectronicDesign?elqTrack=true',
-      title: 'Follow us on Facebook',
-    },
-    {
-      provider: 'linkedin',
-      href: 'https://www.linkedin.com/groups/4210549/?elqTrack=true',
-      title: 'Follow us on LinkedIn',
-    },
-  ],
 };
