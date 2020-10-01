4a75cb9b467fa904f64bacb776ffdca1315c788d
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-10-01 08:10:11 -0500

    asumag alphaThemeConfig setup

--- tenants/asumag/config/core.js
+++ tenants/asumag/config/core.js
@@ -4,24 +4,5 @@ module.exports = {
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
-    manageSubscriptions: 'https://endeavor.dragonforms.com/ASUPrefPage?r=@{encrypted_customer_id}@&pk=NLFooter',
-    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=ASUnewpref',
   },
-  socialMediaLinks: [
-    {
-      provider: 'twitter',
-      href: 'https://twitter.com/ASU_mag?elqTrack=true',
-      title: 'Follow us on Twitter',
-    },
-    {
-      provider: 'facebook',
-      href: 'https://www.facebook.com/asumag?elqTrack=true',
-      title: 'Follow us on Facebook',
-    },
-    // {
-    //   provider: 'linkedin',
-    //   href: '',
-    //   title: 'Follow us on LinkedIn',
-    // },
-  ],
 };
