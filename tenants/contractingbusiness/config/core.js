89f0d73e508f605260e39f5d08f86f10a187c402
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-10-01 08:10:30 -0500

    contractingbusiness alphaThemeConfig setup

--- tenants/contractingbusiness/config/core.js
+++ tenants/contractingbusiness/config/core.js
@@ -4,24 +4,5 @@ module.exports = {
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
-    manageSubscriptions: 'https://endeavor.dragonforms.com/CBPrefPage?r=@{encrypted_customer_id}@&pk=NLFooter',
-    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=CBnewpref',
   },
-  socialMediaLinks: [
-    {
-      provider: 'twitter',
-      href: 'https://twitter.com/contractingbiz/?elqTrack=true',
-      title: 'Follow us on Twitter',
-    },
-    {
-      provider: 'facebook',
-      href: 'https://www.facebook.com/CBMag/?elqTrack=true',
-      title: 'Follow us on Facebook',
-    },
-    {
-      provider: 'linkedin',
-      href: 'https://www.linkedin.com/groups/4078340/profile?elqTrack=true',
-      title: 'Follow us on LinkedIn',
-    },
-  ],
 };
