61740a24ae159ffaa6f96cd386c676900bce057b
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-10-01 08:45:01 -0500

    ehstoday alphaThemeConfig setup

--- tenants/ehstoday/config/core.js
+++ tenants/ehstoday/config/core.js
@@ -4,19 +4,5 @@ module.exports = {
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
-    manageSubscriptions: 'https://endeavor.dragonforms.com/EHSPrefPage?r=@{encrypted_customer_id}@&pk=NLFooter',
-    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=EHSnewpref',
   },
-  socialMediaLinks: [
-    {
-      provider: 'twitter',
-      href: 'https://twitter.com/?elqTrack=true#!/EHSToday',
-      title: 'Follow us on Twitter',
-    },
-    {
-      provider: 'facebook',
-      href: 'https://www.facebook.com/pages/EHS-Today/66542218626?ref=ts&elqTrack=true',
-      title: 'Follow us on Facebook',
-    },
-  ],
 };
