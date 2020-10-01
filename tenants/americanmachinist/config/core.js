6c45694d1443f67e7b7132113e24799676657fef
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-10-01 08:09:59 -0500

    American Machinist alphaThemeConfig setup

--- tenants/americanmachinist/config/core.js
+++ tenants/americanmachinist/config/core.js
@@ -4,24 +4,5 @@ module.exports = {
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
-    manageSubscriptions: 'https://endeavor.dragonforms.com/AMMPrefPage?r=@{encrypted_customer_id}@&pk=NLFooter',
-    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=AMMnewpref',
   },
-  socialMediaLinks: [
-    {
-      provider: 'twitter',
-      href: 'https://twitter.com/?elqTrack=true#!/AmericanMachnst',
-      title: 'Follow us on Twitter',
-    },
-    {
-      provider: 'facebook',
-      href: 'https://www.facebook.com/AmericanMachinist?elqTrack=true',
-      title: 'Follow us on Facebook',
-    },
-    {
-      provider: 'linkedin',
-      href: 'https://www.linkedin.com/groups?trk=anet_ug_grppro&gid=2880425&about=&elqTrack=true',
-      title: 'Follow us on LinkedIn',
-    },
-  ],
 };
