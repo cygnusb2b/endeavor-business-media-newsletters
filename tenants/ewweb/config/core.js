9f533dd285e9872e1105b730795af49280d6a173
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-10-01 10:09:59 -0500

    ewweb emailThemeAlpha setup

--- tenants/ewweb/config/core.js
+++ tenants/ewweb/config/core.js
@@ -4,24 +4,5 @@ module.exports = {
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
-    manageSubscriptions: 'https://endeavor.dragonforms.com/EWPrefPage?r=@{encrypted_customer_id}@&pk=NLFooter',
-    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=EWnewpref',
   },
-  socialMediaLinks: [
-    {
-      provider: 'twitter',
-      href: 'https://twitter.com/ElecWholesaling?elqTrack=true',
-      title: 'Follow us on Twitter',
-    },
-    {
-      provider: 'facebook',
-      href: 'https://www.facebook.com/ElectricalConstructionMaintenanceMagazine?elqTrack=true',
-      title: 'Follow us on Facebook',
-    },
-    {
-      provider: 'linkedin',
-      href: 'https://www.linkedin.com/groups/2685562?elqTrack=true',
-      title: 'Follow us on LinkedIn',
-    },
-  ],
 };
