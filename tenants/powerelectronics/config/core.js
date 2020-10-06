c78426b2472e1c652f98d6ebe4ea86fc98582a72
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-10-06 16:07:13 -0500

    Powerelectronics emailThemeAlpha setup

--- tenants/powerelectronics/config/core.js
+++ tenants/powerelectronics/config/core.js
@@ -12,19 +12,5 @@ module.exports = {
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
-    manageSubscriptions: 'https://endeavor.dragonforms.com/PWEPrefPage?r=@{encrypted_customer_id}@&pk=NLFooter',
-    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=PWEnewPref',
   },
-  socialMediaLinks: [
-    {
-      provider: 'twitter',
-      href: 'https://twitter.com/PowerElecTech?elqTrack=true',
-      title: 'Follow us on Twitter',
-    },
-    {
-      provider: 'facebook',
-      href: 'https://www.facebook.com/powerelectronicstech?elqTrack=true',
-      title: 'Follow us on Facebook',
-    },
-  ],
 };
