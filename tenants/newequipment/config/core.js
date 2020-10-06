89a1c7dcc22e7366294c54a3a7ddc40d435aebf8
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-10-06 16:07:04 -0500

    Newequipment emailThemeAlpha setup

--- tenants/newequipment/config/core.js
+++ tenants/newequipment/config/core.js
@@ -12,24 +12,5 @@ module.exports = {
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
-    manageSubscriptions: 'https://endeavor.dragonforms.com/NEDPrefPage?r=@{encrypted_customer_id}@&pk=NLFooter',
-    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=NEDnewpref',
   },
-  socialMediaLinks: [
-    {
-      provider: 'twitter',
-      href: 'https://twitter.com/?elqTrack=true#!/newequipment',
-      title: 'Follow us on Twitter',
-    },
-    {
-      provider: 'facebook',
-      href: 'https://www.facebook.com/NewEquipment?elqTrack=true',
-      title: 'Follow us on Facebook',
-    },
-    {
-      provider: 'linkedin',
-      href: 'https://www.linkedin.com/grp/home?gid=4850947&elqTrack=true',
-      title: 'Follow us on LinkedIn',
-    },
-  ],
 };
