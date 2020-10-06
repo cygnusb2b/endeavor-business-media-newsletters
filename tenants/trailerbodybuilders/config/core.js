1544beb0b110377212ba212a847df2eee1a93820
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-10-06 16:05:44 -0500

    Trailerbodybuilders emailThemeAlpha setup

--- tenants/trailerbodybuilders/config/core.js
+++ tenants/trailerbodybuilders/config/core.js
@@ -12,24 +12,5 @@ module.exports = {
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
-    manageSubscriptions: 'https://endeavor.dragonforms.com/TBBPrefPage?r=@{encrypted_customer_id}@&pk=NLFooter',
-    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=TBBnewPref',
   },
-  socialMediaLinks: [
-    {
-      provider: 'twitter',
-      href: 'https://twitter.com/?elqTrack=true#!/trailerbb',
-      title: 'Follow us on Twitter',
-    },
-    {
-      provider: 'facebook',
-      href: 'https://www.facebook.com/trailerbodybuilders?elqTrack=true',
-      title: 'Follow us on Facebook',
-    },
-    // {
-    //   provider: 'linkedin',
-    //   href: '',
-    //   title: 'Follow us on LinkedIn',
-    // },
-  ],
 };
