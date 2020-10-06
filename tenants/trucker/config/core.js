7b9b2268ce4c648e6c4159e53ec942f26877c83c
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-10-06 16:05:29 -0500

    Trucker emailThemeAlpha setup

--- tenants/trucker/config/core.js
+++ tenants/trucker/config/core.js
@@ -12,19 +12,5 @@ module.exports = {
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
-    manageSubscriptions: 'https://endeavor.dragonforms.com/AMTKPrefPage?r=@{encrypted_customer_id}@&pk=NLFooter',
-    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=AMTKnewPref',
   },
-  socialMediaLinks: [
-    {
-      provider: 'twitter',
-      href: 'https://twitter.com/truckerAMT?elqTrack=true',
-      title: 'Follow us on Twitter',
-    },
-    {
-      provider: 'facebook',
-      href: 'https://www.facebook.com/pages/American-Trucker-Magazine/37172697238?elqTrack=true',
-      title: 'Follow us on Facebook',
-    },
-  ],
 };
