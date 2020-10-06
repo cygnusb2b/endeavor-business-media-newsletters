3ebdaf3f38e66bdf05a47525ad788a8b251259ef
Caitlyn Knudsvig <cknudsvig@southcomm.com>
2020-10-06 16:06:00 -0500

    Industryweek emailThemeAlpha setup

--- tenants/industryweek/config/core.js
+++ tenants/industryweek/config/core.js
@@ -12,24 +12,5 @@ module.exports = {
     safeSenders: 'news.endeavorb2b.com and mail.endeavorb2b.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
-    manageSubscriptions: 'https://endeavor.dragonforms.com/IDWPrefPage?r=@{encrypted_customer_id}@&pk=NLFooter',
-    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=IDWnewPref',
   },
-  socialMediaLinks: [
-    {
-      provider: 'twitter',
-      href: 'http://twitter.com/?elqTrack=true#!/IndustryWeek',
-      title: 'Follow us on Twitter',
-    },
-    {
-      provider: 'facebook',
-      href: 'http://www.facebook.com/pages/IndustryWeek/91310488418',
-      title: 'Follow us on Facebook',
-    },
-    {
-      provider: 'linkedin',
-      href: 'https://www.linkedin.com/groups/147721/profile',
-      title: 'Follow us on LinkedIn',
-    },
-  ],
 };
