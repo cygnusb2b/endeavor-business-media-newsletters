9d9a649bc3e6c980fe8053e04b9da7d453c89574
Alysha Buselt <Alixers505@users.noreply.github.com>
2020-11-06 09:34:33 -0500

    Added leaderboard, updated style-b header to use config

--- tenants/plasticsmachinerymagazine/config/email-x.js
+++ tenants/plasticsmachinerymagazine/config/email-x.js
@@ -2,4 +2,13 @@ const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config'
 
 const config = new EmailXConfiguration('https://ebm.serve.email-x.io');
 
+config
+  .setAdUnits('journal-of-blow-molding-update', [
+    {
+      name: 'leaderboardPrimary',
+      id: '5fa55a3ff994cd366353e024',
+      width: 670,
+      height: 90,
+    },
+  ]);
 module.exports = config;
