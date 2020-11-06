ea719768b66bc2e771c37dbce54f8eace8bdf23b
Alysha Buselt <Alixers505@users.noreply.github.com>
2020-11-06 10:55:11 -0500

    Adjustment to use scheduled section instead of emailx

--- tenants/plasticsmachinerymagazine/config/email-x.js
+++ tenants/plasticsmachinerymagazine/config/email-x.js
@@ -2,13 +2,4 @@ const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config'
 
 const config = new EmailXConfiguration('https://ebm.serve.email-x.io');
 
-config
-  .setAdUnits('journal-of-blow-molding-update', [
-    {
-      name: 'leaderboardPrimary',
-      id: '5fa55a3ff994cd366353e024',
-      width: 670,
-      height: 90,
-    },
-  ]);
 module.exports = config;
