35d928aec0a5ad6b192f1962cec1724dc6c696f5
Alysha Buselt <Alixers505@users.noreply.github.com>
2021-01-13 15:23:39 -0500

    Missed updating the address for PMM
    Per https://southcomm.atlassian.net/browse/DEV-415

--- tenants/plasticsmachinerymanufacturing/config/core.js
+++ tenants/plasticsmachinerymanufacturing/config/core.js
@@ -8,7 +8,7 @@ module.exports = {
     },
   },
   optOut: {
-    address: 'Plastics Machinery & Manufacturing Magazine <br>2 Summit Park, Suite 300, Third Floor <br>Independence, OH 44131',
+    address: 'Plastics Machinery & Manufacturing Magazine<br>Endeavor Business Media<br>331 54th Avenue N.<br>Nashville, TN 37209',
     safeSenders: 'news.plasticsmachinerymagazine.com and mail.plasticsmachinerymagazine.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
