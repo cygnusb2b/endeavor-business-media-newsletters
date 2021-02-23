6fa07f217a3def148192613baf1059b19cc5e645
Alysha Buselt <Alixers505@users.noreply.github.com>
2021-02-23 18:07:09 -0500

    Creation of SmartBuildingsTech tenant
    Per https://southcomm.atlassian.net/browse/DEV-536

---
+++ tenants/smartbuildingstech/config/core.js
@@ -0,0 +1,58 @@
+module.exports = {
+  dpm: {
+    emailx: {
+      enabled: true,
+    },
+    content: {
+      enabled: true,
+    },
+  },
+  optOut: {
+    address: 'Smart Buildings Technology<br />Endeavor Business Media<br />331 54th Avenue N.<br />Nashville, TN 37209',
+    safeSenders: 'news.smartbuildingstech.com and mail.smartbuildingstech.com',
+    privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
+    phoneNumber: '800-547-7377',
+    // manageSubscriptions: 'https://endeavor.dragonforms.com/loading.do?omedasite=RDHPrefPage',
+    // signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=RDHnewpref',
+  },
+  // socialMediaLinks: [
+  //   {
+  //     provider: 'twitter',
+  //     href: 'https://twitter.com/rdhmag?lang=en',
+  //     title: 'Follow us on Twitter',
+  //   },
+  //   {
+  //     provider: 'facebook',
+  //     href: 'https://www.facebook.com/RDHMagazine/',
+  //     title: 'Follow us on Facebook',
+  //   },
+  //   {
+  //     provider: 'instagram',
+  //     href: 'https://www.instagram.com/rdhmag/?hl=en',
+  //     title: 'Follow us on LinkedIn',
+  //   },
+  //   {
+  //     provider: 'youtube',
+  //     href: 'https://www.youtube.com/channel/UCtOxclLyg6O0Eb5kRXqk9sA',
+  //     title: 'Follow us on LinkedIn',
+  //   },
+  // ],
+  brandNavLinks: [
+    {
+      href: 'https://www.smartbuildingstech.com/',
+      title: 'Home',
+    },
+    {
+      href: 'https://www.smartbuildingstech.com/contact-us',
+      title: 'Contact Us',
+    },
+    {
+      href: 'https://www.smartbuildingstech.com/subscribe',
+      title: 'Subscribe',
+    },
+    {
+      href: 'https://www.smartbuildingstech.com/page/advertise',
+      title: 'Advertise',
+    },
+  ],
+};
