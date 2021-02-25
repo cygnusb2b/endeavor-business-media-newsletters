67607b777ced5566009c1ba694adaddd6e1f82fd
Alysha Buselt <Alixers505@users.noreply.github.com>
2021-02-25 12:10:57 -0500

    removed unneeded code

--- tenants/smartbuildingstech/config/core.js
+++ tenants/smartbuildingstech/config/core.js
@@ -12,47 +12,11 @@ module.exports = {
     safeSenders: 'news.smartbuildingstech.com and mail.smartbuildingstech.com',
     privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
     phoneNumber: '800-547-7377',
-    manageSubscriptions: 'https://endeavor.dragonforms.com/loading.do?omedasite=SBTprefpage',
-    signUp: 'https://endeavor.dragonforms.com/loading.do?omedasite=SBTNewPref&pk=NLFooter',
   },
-  // socialMediaLinks: [
-  //   {
-  //     provider: 'twitter',
-  //     href: 'https://twitter.com/rdhmag?lang=en',
-  //     title: 'Follow us on Twitter',
-  //   },
-  //   {
-  //     provider: 'facebook',
-  //     href: 'https://www.facebook.com/RDHMagazine/',
-  //     title: 'Follow us on Facebook',
-  //   },
-  //   {
-  //     provider: 'instagram',
-  //     href: 'https://www.instagram.com/rdhmag/?hl=en',
-  //     title: 'Follow us on LinkedIn',
-  //   },
-  //   {
-  //     provider: 'youtube',
-  //     href: 'https://www.youtube.com/channel/UCtOxclLyg6O0Eb5kRXqk9sA',
-  //     title: 'Follow us on LinkedIn',
-  //   },
-  // ],
   brandNavLinks: [
     {
       href: 'https://www.smartbuildingstech.com/',
       title: 'Home',
-    },
-    {
-      href: 'https://www.smartbuildingstech.com/contact-us',
-      title: 'Contact Us',
-    },
-    {
-      href: 'https://www.smartbuildingstech.com/subscribe',
-      title: 'Subscribe',
-    },
-    {
-      href: 'https://www.smartbuildingstech.com/page/advertise',
-      title: 'Advertise',
     },
   ],
 };
