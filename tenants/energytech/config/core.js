69e6161e7725f8dc61a48093971765ab7fa59e00
Jeremy Laird <Jeremy.Laird@cygnus.com>
2021-09-08 13:35:28 -0500

    Fixed quotes

--- tenants/energytech/config/core.js
+++ tenants/energytech/config/core.js
@@ -6,15 +6,15 @@ module.exports = {
   },
   optOut: {
     address:
-      "Energy Tech<br />Endeavor Business Media<br />331 54th Avenue N.<br />Nashville, TN 37209",
-    safeSenders: "news.energytech.com and mail.energytech.com",
-    privacyPolicyUrl: "https://www.endeavorbusinessmedia.com/privacy-policy",
-    phoneNumber: "800-547-7377",
+      'Energy Tech<br />Endeavor Business Media<br />331 54th Avenue N.<br />Nashville, TN 37209',
+    safeSenders: 'news.energytech.com and mail.energytech.com',
+    privacyPolicyUrl: 'https://www.endeavorbusinessmedia.com/privacy-policy',
+    phoneNumber: '800-547-7377',
   },
   brandNavLinks: [
     {
-      href: "https://www.energytech.com/",
-      title: "Home",
+      href: 'https://www.energytech.com/',
+      title: 'Home',
     },
   ],
 };
